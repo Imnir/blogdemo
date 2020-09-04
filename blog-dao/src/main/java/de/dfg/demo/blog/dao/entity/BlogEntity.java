a12f28acfd27bdca04f54950c17e4c2776b6f470
sgiern2s <RjeA7exm>
2020-09-04 13:16:19 +0200

    BlogEntity erstellt

--- blog-dao/src/main/java/de/dfg/demo/blog/dao/entity/BlogEntity.java
+++ blog-dao/src/main/java/de/dfg/demo/blog/dao/entity/BlogEntity.java
@@ -23,202 +23,67 @@ import java.time.LocalDate;
  * The Class AntragEntity.
  */
 @Entity
-@NamedQuery(name = BlogEntity.QUERY_BY_ID, query = "SELECT e FROM AntragEntity e WHERE e.id = :" + BlogEntity.QUERY_PARAMETER_ID)
-@NamedQuery(name = BlogEntity.QUERY_BY_TITELD, query = "SELECT e FROM AntragEntity e WHERE e.titelDeutsch LIKE :" + BlogEntity.QUERY_PARAMETER_TITELD)
-@Table(name = "ANTRAG")
+@NamedQuery(name = BlogEntity.QUERY_BY_TITEL, query = "SELECT e FROM BlogEntity e WHERE e.titelDeutsch LIKE :" + BlogEntity.QUERY_PARAMETER_TITEL)
+@Table(name = "Blog")
 public class BlogEntity implements Serializable {
 
 	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = 1L;
 
 	/** The Constant QUERY_BY_TITELD. */
-	public static final String QUERY_BY_TITELD = "AntragEntity.QUERY_BY_TITELD";
-	
-	/** The Constant QUERY_BY_ID. */
-	public static final String QUERY_BY_ID = "AntragEntity.QUERY_BY_ID";
+	public static final String QUERY_BY_TITEL = "AntragEntity.QUERY_BY_TITEL";
+
 	
 	/** The Constant QUERY_PARAMETER_TITELD. */
-	public static final String QUERY_PARAMETER_TITELD = "QUERY_PARAMETER_TITELD";
-	
-	/** The Constant QUERY_PARAMETER_ID. */
-	public static final String QUERY_PARAMETER_ID = "QUERY_PARAMETER_ID";
+	public static final String QUERY_PARAMETER_TITEL = "QUERY_PARAMETER_TITEL";
 
 
-	/** The id. */
-	@Id
-	@Column(name = "ANT_ID", nullable=false)
-	private long id;
+	/** The titel */
+	@Column(name="BLOG_TITEL")
+	private String titel;
 
-	/** The titel deutsch. */
-	@Column(name = "ANT_TITELD")
-	private String titelDeutsch = "";		// ANTRAG.ANT_TITELD
+	/** The Date */
+	@Column(name = "BLOG_DATE")
+	private LocalDate date;
 
-	/** The titel englisch. */
-	@Column(name = "ANT_TITELE")
-	private String titelEnglisch = "";		// ANTRAG.ANT_TITELE
+	/** The author */
+	@Column(name="BLOG_AUTHOR")
+	private String author;
 
-	/** The eingangs datum. */
-	@Column(name = "ANT_EINGANGSDATUM")
-	private LocalDate eingangsDatum;		// ANTRAG.ANT_EINGANGSDATUM
+	/** The Blogtext */
+	@Column(name="BLOG_TEXT")
+	private String text;
 
-	/** The antrag dauer. */
-	@Column(name = "ANT_ANTRAGDAUER") // ANTRAG.ANT_ANTRAGDAUER
-	private Long antragDauer;
-
-	/** The antrag summe. */
-	@Column(name = "ANT_ANTRAGSUMME") // ANTRAG.ANT_ANTRAGSUMME
-	private Double antragSumme;
-
-	/** The bewilligung dauer. */
-	@Column(name = "ANT_BEWILLIGUNGDAUER")
-	private Long bewilligungDauer; // ANTRAG.ANT_BEWILLIGUNGSDAUER
-
-	/** The bewilligung summe. */
-	@Column(name = "ANT_BEWILLIGUNGSUMME")
-	private Double bewilligungSumme;		// ANTRAG.ANT_BEWILLIGUNGSUMME
-
-	/**
-	 * Gets the id.
-	 *
-	 * @return the id
-	 */
-	public long getId() {
-		return id;
+	public LocalDate getDate() {
+		return date;
 	}
 
-	/**
-	 * Sets the id.
-	 *
-	 * @param id the new id
-	 */
-	public void setId(long id) {
-		this.id = id;
+	public void setDate(LocalDate date) {
+		this.date = date;
 	}
 
-	/**
-	 * Gets the titel deutsch.
-	 *
-	 * @return the titel deutsch
-	 */
-	public String getTitelDeutsch() {
-		return titelDeutsch;
+	public String getAuthor() {
+		return author;
 	}
 
-	/**
-	 * Sets the titel deutsch.
-	 *
-	 * @param titelDeutsch the new titel deutsch
-	 */
-	public void setTitelDeutsch(String titelDeutsch) {
-		this.titelDeutsch = titelDeutsch;
+	public void setAuthor(String author) {
+		this.author = author;
 	}
 
-	/**
-	 * Gets the titel englisch.
-	 *
-	 * @return the titel englisch
-	 */
-	public String getTitelEnglisch() {
-		return titelEnglisch;
+	public String getText() {
+		return text;
 	}
 
-	/**
-	 * Sets the titel englisch.
-	 *
-	 * @param titelEnglisch the new titel englisch
-	 */
-	public void setTitelEnglisch(String titelEnglisch) {
-		this.titelEnglisch = titelEnglisch;
+	public void setText(String text) {
+		this.text = text;
 	}
 
-	/**
-	 * Gets the eingangs datum.
-	 *
-	 * @return the eingangs datum
-	 */
-	public LocalDate getEingangsDatum() {
-		return eingangsDatum;
+	public String getTitel() {
+		return titel;
 	}
 
-	/**
-	 * Sets the eingangs datum.
-	 *
-	 * @param eingangsDatum the new eingangs datum
-	 */
-	public void setEingangsDatum(LocalDate eingangsDatum) {
-		this.eingangsDatum = eingangsDatum;
-	}
-
-	/**
-	 * Gets the antrag dauer.
-	 *
-	 * @return the antrag dauer
-	 */
-	public Long getAntragDauer() {
-		return antragDauer;
-	}
-
-	/**
-	 * Sets the antrag dauer.
-	 *
-	 * @param antragDauer the new antrag dauer
-	 */
-	public void setAntragDauer(Long antragDauer) {
-		this.antragDauer = antragDauer;
-	}
-
-	/**
-	 * Gets the antrag summe.
-	 *
-	 * @return the antrag summe
-	 */
-	public Double getAntragSumme() {
-		return antragSumme;
-	}
-
-	/**
-	 * Sets the antrag summe.
-	 *
-	 * @param antragSumme the new antrag summe
-	 */
-	public void setAntragSumme(Double antragSumme) {
-		this.antragSumme = antragSumme;
-	}
-
-	/**
-	 * Gets the bewilligung dauer.
-	 *
-	 * @return the bewilligung dauer
-	 */
-	public Long getBewilligungDauer() {
-		return bewilligungDauer;
-	}
-
-	/**
-	 * Sets the bewilligung dauer.
-	 *
-	 * @param bewilligungDauer the new bewilligung dauer
-	 */
-	public void setBewilligungDauer(Long bewilligungDauer) {
-		this.bewilligungDauer = bewilligungDauer;
-	}
-
-	/**
-	 * Gets the bewilligung summe.
-	 *
-	 * @return the bewilligung summe
-	 */
-	public Double getBewilligungSumme() {
-		return bewilligungSumme;
-	}
-
-	/**
-	 * Sets the bewilligung summe.
-	 *
-	 * @param bewilligungSumme the new bewilligung summe
-	 */
-	public void setBewilligungSumme(Double bewilligungSumme) {
-		this.bewilligungSumme = bewilligungSumme;
+	public void setTitel(String titel) {
+		this.titel = titel;
 	}
 
 }
